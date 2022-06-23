f508cc7940400857475ab88047b5e89a5415e7c3
Slava Knyazev <slava@knyazev.io>
2022-06-23 00:06:08 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-[ `whoami` = root ] || exec su -c $0 root
+if [ $# -eq 0 ]
+  then
+    echo "Usage: bash install.sh nginx-full";
+    exit 1;
+fi
+
+[ `whoami` = root ] || exec su -c "$0 $@" root
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8028BE1819F3E4A0
 echo "deb https://nginx-pagespeed.knyz.org/dist/ /" > /etc/apt/sources.list.d/nginx-pagespeed.list
 echo "Package: *" > /etc/apt/preferences.d/99nginx-pagespeed
 echo "Pin: origin nginx-pagespeed.knyz.org" >> /etc/apt/preferences.d/99nginx-pagespeed
 echo "Pin-Priority: 900" >> /etc/apt/preferences.d/99nginx-pagespeed
 apt update
-apt install nginx-full # If NGINX is already installed, an `apt upgrade` works too
+apt install $1
 echo "pagespeed on;" > /etc/nginx/conf.d/pagespeed.conf
 echo "pagespeed FileCachePath \"/var/cache/pagespeed/\";" >> /etc/nginx/conf.d/pagespeed.conf
 echo "pagespeed FileCacheSizeKb 102400;" >> /etc/nginx/conf.d/pagespeed.conf
